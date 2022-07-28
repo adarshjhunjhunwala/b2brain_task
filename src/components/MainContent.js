38084556e78fee0ce711226d355b3890bb5e19f4
adarshjhunjhunwala <adarsh4295@gmail.com>
2022-07-28 16:19:00 +0530

    95% frontend completed

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,14 +2,44 @@ import React from 'react'
 import hero_img from '../assets/images/hero_img.svg'
 import trusted_by from '../assets/images/trusted_by.svg'
 import customers from '../assets/images/customers.svg'
-const MainContent = () => {
-  return (
-    <div className="main-content">
-    <img src={hero_img} alt="hero_img" />
-    <img src={trusted_by} alt="trusted_by" />
-    <img src={customers} alt="customers" />
-    </div>
-  )
+import profile_img from '../assets/images/profile_img.svg'
+
+const MainContent = (props) => {
+    return (
+        <div className="main-content">
+            {/* When not searching for anything */}
+            { !props.isSearch && 
+            <>
+            <img src={hero_img} alt="hero_img" />
+            <img src={trusted_by} alt="trusted_by" />
+            <img src={customers} alt="customers" />
+            </>
+            }
+            {/* When searching for something */}
+            {
+                props.isSearch &&
+            <>
+            <h2 className="similar-accounts-heading">Similar accounts</h2>
+            <div className="row mt-3">
+                {[...Array(7)].map((e, i) => { 
+                    return <div className="column mt-3" key={i}>
+                                <img src={profile_img} className="company-img" alt="profile_img" />
+                                <p className="company-title">Harrow Inc.</p>
+                                <p className="company-text">www.harrow.com
+                                <button href="/" target="_blank" rel="noreferrer" className="btn btn-outline-danger track-btn">Track</button></p>
+                            </div>
+                })}
+            </div>
+            <div className="quick-actions">
+                <h2 className="quick-actions-heading">Quick Actions</h2>
+                <a href="/" className='quick-actions-text'><p>Track new account</p></a>
+                <a href="/" className='quick-actions-text'><p>Bulk track accounts</p></a>
+                <a href="/" className='quick-actions-text'><p>Manage accounts</p></a>
+            </div>
+            </>
+            }
+        </div>
+    )
 }
 
 export default MainContent
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,6 @@ import preferences from '../assets/images/preferences.svg'
 import integrations from '../assets/images/integrations.svg'
 import team from '../assets/images/team.svg'
 import help_support from '../assets/images/help_support.svg'
-import search from '../assets/images/search.svg'
 import notification from '../assets/images/notification.svg'
 import profile_img from '../assets/images/profile_img.svg'
 import MainContent from "./MainContent";
@@ -16,9 +15,14 @@ import MainContent from "./MainContent";
 const Sidebar = () => {
     const [isAccounts, setisAccounts] = useState(true);
     const [isPreferences, setisPreferences] = useState(true);
+    const [isSearch, setisSearch] = useState(false);
 
     function toggle(name) {
         name === "Accounts" ? setisAccounts(prevState => !prevState) : setisPreferences(prevState => !prevState)
+    }
+
+    function isSearchToggle() {
+        setisSearch(prevState => !prevState);
     }
     return (
         <div className="container-fluid h-100">
@@ -57,7 +61,8 @@ const Sidebar = () => {
                 <div className="col-10">
                     <nav className="navbar navbar-expand-lg
                                 navbar-light">
-                        <img src={search} alt="search" />&nbsp;&nbsp;&nbsp;&nbsp;Search by account name or website
+                        {/* <img src={search} alt="search" /> */}
+                        <a onClick={() => isSearchToggle()}><i className={!isSearch ? "fa fa-lg fa-search" : "fa fa-lg fa-times"}></i></a>&nbsp;&nbsp;&nbsp;&nbsp;Search by account name or website
                         <button className="navbar-toggler" type="button"
                             data-toggle="collapse"
                             data-target="#navbarNavAltMarkup"
@@ -68,13 +73,13 @@ const Sidebar = () => {
                         </button>
                         <div className="collapse navbar-collapse"
                             id="navbarNavAltMarkup">
-                            <div className="navbar-nav" style={{ "margin-left": "880px" }}>
+                            <div className="navbar-nav" style={{ "marginLeft": "880px" }}>
                                 <a className="nav-item nav-link" href="/"><img src={notification} alt="notification" /><span className="position-absolute top-2 start-10 p-1 rounded-circle"></span></a>
                                 <a className="nav-item nav-link" href="/"><img src={profile_img} alt="profile_img" /></a>
                             </div>
                         </div>
                     </nav>
-                    <MainContent />
+                    <MainContent isSearch={isSearch}/>
                 </div>
             </div>
         </div>
